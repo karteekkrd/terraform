137b7bdd334ce635b6de74aac78fab706dec5d3f
Karteek Kuridi <karteek.kuridi@Indegene.com>
2019-09-01 19:53:27 +0530

    role assignment module ready

---
+++ roleassignment/variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_id" {
+    description = "sub id"
+}
+
+variable "group_name" {
+    description = "group name"
+}
+
+variable "role_name" {
+    description = "role name"
+}
